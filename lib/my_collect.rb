aa2ec2eb332aecf12dbbe3ec5392cf2eb83c8cab
MC Simoes <mariacristina.simoes@gmail.com>
2018-12-21 03:47:52 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
+def my_collect (array)
+  i = 0 
+  while i < array.length 
+  array.collect do |name|
+   yield array
+   i = i + 1 
+end
+end
 
+my_collect(array) {name.split(" ").first
+    name.upcase} fjeij
 
