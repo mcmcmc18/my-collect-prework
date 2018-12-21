dff2ca25015aefab18bdf7cead6d180e0063bbf6
MC Simoes <mariacristina.simoes@gmail.com>
2018-12-21 03:50:02 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,11 +2,7 @@ def my_collect (array)
   i = 0 
   collection = []
   while i < array.length 
-collection 
-   i = i + 1 
+collection << yield(array[i])
+i = i + 1
 end
 end
-
-my_collect(array) {name.split(" ").first
-    name.upcase}
-
